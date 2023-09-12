6d103046ba9d0f7d9762232794b3bd687e69babd
Matheus Melo <matheusprestesdmelo744@gmail.com>
2023-09-12 11:18:20 +0000

    feat(user): Criação de um usuário

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,25 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { User } from '../entities/user.entity';
+
+export class CreateUserDto extends User {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(125)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
+  password: string;
+
+  @IsString()
+  @MinLength(3)
+  name: string;
+}
