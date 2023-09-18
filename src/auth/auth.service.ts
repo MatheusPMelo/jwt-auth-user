57dee3c7042966efffc0ba79d34648ff5542d2c9
Matheus Melo <matheusprestesdmelo744@gmail.com>
2023-09-18 18:25:47 +0000

    declaraçãod o validateuser

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
+  validateUser(email: string, password: string) {
+    throw new Error('Method not implemented.');
+  }
   async login() {
     return 'fez o login';
   }
